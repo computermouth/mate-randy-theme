249a93c9a9e8ca2b22cb36c72480ba212007565b
computermouth <computermouth@crunchbangplusplus.org>
2015-08-21 22:09:23 -0500

    Added screenshots

--- randyTheme.sh
+++ randyTheme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-RANDR=$(printf "%02x" $(($RANDOM % 106 + 150)))
-RANDG=$(printf "%02x" $(($RANDOM % 106 + 150)))
-RANDB=$(printf "%02x" $(($RANDOM % 106 + 150)))
+RANDR=$(printf "%02x" $(($RANDOM % 156 + 100)))
+RANDG=$(printf "%02x" $(($RANDOM % 156 + 100)))
+RANDB=$(printf "%02x" $(($RANDOM % 156 + 100)))
 
 gsettings set org.mate.background secondary-color "'#"$RANDR$RANDR$RANDG$RANDG$RANDB$RANDB"'"
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color "'#"$RANDR$RANDR$RANDG$RANDG$RANDB$RANDB"'"
